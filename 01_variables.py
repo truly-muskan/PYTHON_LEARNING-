3a6029d5066388330fa54951db7fbd80900e0dc5
MUSKAN <muskan.techie07@gmail.com>
2026-06-15 00:00:36 +0530

    Add variable examples in 01_variables.py

---
+++ 01_variables.py
@@ -0,0 +1,19 @@
+# Example 1
+name = "John"
+print(name)
+
+# Example 2
+age = 21
+print(age)
+
+# Example 3
+height = 5.8
+print(height)
+
+# Example 4
+is_student = True
+print(is_student)
+
+# Example 5
+a, b, c = 10, 20, 30
+print(a, b, c)
